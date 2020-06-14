bebf72477b0ca671a7d2bd11d3d3ab4c81f52dda
Rahul <rahuldev1997@yahoo.com>
2020-06-15 00:02:08 +0530

    Initial Build

---
+++ src/pages/scheduler.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Calendar from "../components/calendar"
+import Layout from './../components/layout';
+
+const Scheduler = () => {
+  return (
+    <Layout>
+      <Calendar />
+    </Layout>
+  )
+}
+
+export default Scheduler
